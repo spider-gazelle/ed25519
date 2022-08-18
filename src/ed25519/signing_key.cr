75c4905433549ca7163447894a21fa29442625e7
Stephen von Takach <steve@place.technology>
2022-08-18 12:23:41 +1000

    fix: use random secure to generate bytes

--- src/ed25519/signing_key.cr
+++ src/ed25519/signing_key.cr
@@ -1,16 +1,9 @@
 require "./verify_key"
 
 class Ed25519::SigningKey
-  def initialize(seed : UInt64 = rand(UInt64::MAX), key_size : Int32 = 32)
-    @seed = seed
-    random = Random.new(seed)
-    @key_bytes = random.random_bytes(key_size)
+  def initialize(@key_bytes : Bytes = Random::Secure.hex(32).hexbytes)
   end
 
-  def initialize(@key_bytes : Bytes)
-  end
-
-  getter seed : UInt64? = nil
   getter key_bytes : Bytes
   getter verify_key : VerifyKey { VerifyKey.new(Ed25519.getPublicKey(@key_bytes)) }
 
